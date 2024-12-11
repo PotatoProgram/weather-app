26ef17b618f84c14407e24260f6a4f9aa2efc8f7
PotatoProgram <101606844+PotatoProgram@users.noreply.github.com>
2024-12-11 13:30:17 -0500

    added api call

--- app.js
+++ app.js
@@ -7,4 +7,11 @@ function handleSearchSubmit(event) {
   console.log(searchInput.value);
   let cityElement = document.querySelector("#selectedCity");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
+}
+
+function searchCity(city) {
+  //make an api call and update the interface of the page
+  let apiKey = "a3o950fc274379347b6a44aft08a3cb0"
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 }
